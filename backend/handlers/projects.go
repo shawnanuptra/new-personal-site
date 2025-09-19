2621a357c66694581a812576877855ac68a6674a
Shawn Martin <shawn.martin@salesfire.com>
2025-09-19 23:04:32 +0100

    added first projects handler that returns 4 latest projects

--- backend/handlers/projects.go
+++ backend/handlers/projects.go
@@ -2,11 +2,50 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"os"
 )
+
+type SanityResponse struct {
+	Result []any `json:"result"`
+}
+
+func SanityAPI(query string) string {
+	baseURL := fmt.Sprintf(
+		"https://%s.api.sanity.io/%s/data/query/production",
+		os.Getenv("SANITY_PROJECT_ID"),
+		os.Getenv("SANITY_API_VERSION"),
+	)
+
+	params := url.Values{}
+	params.Add("query", query)
+	params.Add("returnQuery", "true")
+
+	return fmt.Sprintf("%s?%s", baseURL, params.Encode())
+}
 
 func AllProjects(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(map[string]string{"message": "hitting projects"})
+	query := "*[_type=='project'] | order(_updatedAt desc)[0...4] | {title, slug, description, thumbnail}"
+
+	res, err := http.Get(SanityAPI(query))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Server Error. Failed to fetch from Sanity API."})
+		return
+	}
+	defer res.Body.Close()
+
+	var sanityResponse SanityResponse
+	if err := json.NewDecoder(res.Body).Decode(&sanityResponse); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": err.Error()})
+		return
+	}
+
+	// only return the `result` field
+	json.NewEncoder(w).Encode(map[string][]any{"projects": sanityResponse.Result})
 }
--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ fetch }) => {
 
     const response = await fetch('api/projects')
-    const { message: projects } = await response.json()
+    const { projects } = await response.json()
 
     return {
         projects,
