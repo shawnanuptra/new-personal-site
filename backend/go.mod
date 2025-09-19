c8ddf164025ebff10f71d63cc5d9c4e5e0c352d9
Shawn Martin <shawn.martin@salesfire.com>
2025-09-19 22:00:27 +0100

    added env handling

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/shawnanuptra/new-personal-site/backend
 
 go 1.25.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/shawnanuptra/new-personal-site/backend/handlers"
 )
 
 func main() {
+	// get APP_ENV, default to local
+	env := os.Getenv("APP_ENV")
+
+	if env == "" {
+		env = "local"
+	}
+
+	// attempt to load the env file, crash if fails
+	if err := godotenv.Load(fmt.Sprintf(".env.%s", env)); err != nil {
+		log.Fatal(err)
+	}
+
 	router := http.NewServeMux()
 
 	router.HandleFunc("/ping", handlers.Ping)
