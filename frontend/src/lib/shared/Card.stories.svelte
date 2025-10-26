4e976110f9247423db55fd0003fe6ccb98944f02
Shawn Martin <shawnanuptraamartin@gmail.com>
2025-10-26 22:36:40 +0000

    update story

--- frontend/src/lib/shared/Card.stories.svelte
+++ frontend/src/lib/shared/Card.stories.svelte
@@ -3,7 +3,16 @@
 	import Card from './Card.svelte';
 	import { fn } from 'storybook/test';
 
-	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const mockProject = {
+		title: 'Project Title',
+		slug: 'project-title',
+		description: "This is the card's description",
+		thumbnailUrl:
+			'https://thunderdungeon.com/wp-content/uploads/2025/04/the-lion-memes-18-20250415.jpg',
+		publishedAt: '2025-08-03',
+		content: "This shouldn't be here!",
+	};
+
 	const { Story } = defineMeta({
 		title: 'Shared/Card',
 		component: Card,
@@ -17,19 +26,32 @@
 	});
 </script>
 
-<Story
-	name="Project"
-	args={{
-		project: {
-			title: 'Project Title',
-			slug: 'project-title',
-			description: "This is the card's description",
-			thumbnailUrl:
-				'https://thunderdungeon.com/wp-content/uploads/2025/04/the-lion-memes-18-20250415.jpg',
-			publishedAt: '2025-08-03',
-			series: 'project-test',
-			entry: 1,
-			content: "This shouldn't be here!",
-		},
-	}}
-/>
+<Story name="Single Card" args={{ project: mockProject }} />
+
+<Story name="Grid of Cards" asChild>
+	<div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 20px;">
+		<Card
+			project={{
+				title: 'Project Title',
+				slug: 'project-title',
+				description: "This is the card's description",
+				thumbnailUrl:
+					'https://thunderdungeon.com/wp-content/uploads/2025/04/the-lion-memes-18-20250415.jpg',
+				publishedAt: '2025-08-03',
+				content: "This shouldn't be here!",
+			}}
+		/>
+		<Card
+			project={{
+				title: 'Project Title Longer Longer',
+				slug: 'project-title',
+				description:
+					"This is the card's descriptionThis shouldn't be here!This shouldn't be here!This shouldn't be here!This shouldn't be here!This shouldn't be here!This shouldn't be here!This shouldn't be here!",
+				thumbnailUrl:
+					'https://thunderdungeon.com/wp-content/uploads/2025/04/the-lion-memes-18-20250415.jpg',
+				publishedAt: '2025-08-03',
+				content: "This shouldn't be here!",
+			}}
+		/>
+	</div>
+</Story>
