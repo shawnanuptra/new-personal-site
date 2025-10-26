f0e75b261b792df181ab12057e4161521939f6e0
Shawn Martin <shawnanuptraamartin@gmail.com>
2025-10-26 18:14:48 +0000

    updated Card.svelte

--- frontend/src/lib/shared/Card.svelte
+++ frontend/src/lib/shared/Card.svelte
@@ -5,12 +5,10 @@
 </script>
 
 <article class="card">
-	<div>
-		<div class="card-title-wrapper">
-			<a class="card-title" href={'project/' + project.slug}>{project.title}</a>
-		</div>
-		<p>{project.description}</p>
+	<div class="card-title-wrapper">
+		<a class="card-title" href={'project/' + project.slug}>{project.title}</a>
 	</div>
+	<p class="card-content">{project.description}</p>
 	<img
 		class="card-thumbnail"
 		src={project.thumbnailUrl + '?w=750&fit=max'}
@@ -22,14 +20,15 @@
 <style>
 	.card {
 		position: relative;
-		width: 100%;
-		display: flex;
-		flex-direction: column-reverse;
+		display: grid;
+		grid-row: 3 span;
+		grid-template-rows: subgrid;
+		gap: 1rem;
+
 		border: 2px dashed var(--black);
 		border-radius: var(--border-radius-md);
-		padding: 1rem 1.25rem;
+		padding: 2rem 1rem;
 		transition: all 150ms ease-in-out;
-		justify-content: space-between;
 
 		&:hover,
 		&:focus-within {
@@ -39,20 +38,31 @@
 		}
 	}
 
+	.card-thumbnail {
+		order: 1;
+		max-height: 15rem;
+		margin: auto;
+	}
+
+	.card-content {
+		order: 3;
+		padding: 0 0.5rem;
+	}
+
 	.card-title-wrapper {
+		order: 2;
 		display: inline-block;
 		transition: all 150ms 300ms ease-in-out;
 		border: 2px solid transparent;
 		border-radius: var(--border-radius-md);
-		padding: 0.5rem;
+		color: var(--black);
+		margin: auto 0;
+		padding: 0.25rem 0.5rem;
 
 		&:has(> .card-title:hover) {
-			background-color: orange;
-
 			border: 2px solid var(--black);
 			text-decoration-style: solid;
 
-			margin-bottom: 1rem;
 			box-shadow: var(--box-shadow);
 		}
 	}
@@ -61,6 +71,8 @@
 		font-size: 2.5rem;
 		font-weight: bold;
 		text-decoration-style: dashed;
+		color: var(--black);
+		transition: color 150ms 300ms ease-in-out;
 
 		/* stretch the hitbox */
 		&::after {
@@ -70,7 +82,6 @@
 			z-index: 1;
 		}
 
-		padding: 0.25rem;
 		margin: 1rem 0;
 		display: inline;
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -48,7 +48,9 @@
 
 	.cards {
 		display: grid;
-		grid-template-columns: 1fr 1fr;
-		gap: 2rem;
+		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		gap: 1.5rem;
+
+		grid-template-rows: auto 1fr auto;
 	}
 </style>
